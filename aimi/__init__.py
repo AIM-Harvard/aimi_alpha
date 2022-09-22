d30ab81143bd5dcc78f742827147fedf638fd084
Den <dennis_bontempi@dfci.harvard.edu>
2022-09-22 18:07:07 +0200

    Fix __init__

--- aimi/__init__.py
+++ aimi/__init__.py
@@ -4,6 +4,7 @@ from .platipy import *
 from .totalsegmentator import *
 
 from .nnunet_pancreas import *
+from .nnunet_liver import *
 from .nnunet_nsclc import *
 from .nnunet_segthor import *
 from .nnunet_abdominal_oar import *
