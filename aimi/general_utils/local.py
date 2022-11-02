f8fed8d4ba69ec2014d761bac0b50fcb557dfb80
denbonte <dennis_bontempi@dfci.harvard.edu>
2022-11-02 12:17:28 +0100

    Update totalsegmentator run.py and sorting

--- aimi/general_utils/local.py
+++ aimi/general_utils/local.py
@@ -16,7 +16,8 @@ import shutil
 import subprocess
 
 
-def sort_patient_data(raw_base_path, sorted_base_path, path_to_dicomsort_repo):
+def sort_patient_data(raw_base_path, sorted_base_path, path_to_dicomsort_repo,
+                      sorted_structure = "%%SeriesInstanceUID/%%SOPInstanceUID.dcm"):
 
   """
   Download raw DICOM data and run dicomsort to standardise the input format.
@@ -28,6 +29,8 @@ def sort_patient_data(raw_base_path, sorted_base_path, path_to_dicomsort_repo):
                                   This folder should be internal and not exposed to the local node (disposable)
     remove_raw       : optional - whether to remove or not the raw non-sorted data
                                   (after sorting with dicomsort). Defaults to True.
+    sorted_structure : optional - structure of the sorted directory.
+                                  Defaults to $sorted_base_path/$SeriesInstanceUID/$SOPInstanceUID.dcm
     
     # FIXME - get rid of this once DICOMsort gets released as a package (... soon)!
     path_to_dicomsort_repo : optional - path to the folder where the dicomsort
@@ -42,7 +45,7 @@ def sort_patient_data(raw_base_path, sorted_base_path, path_to_dicomsort_repo):
   dicomsort_py_path = os.path.join(path_to_dicomsort_repo, "dicomsort/dicomsort.py")
   bash_command = list()
   bash_command += ["python", "%s"%dicomsort_py_path, "-k", "-u",
-                   "%s"%raw_base_path, "%s/%%PatientID/%%Modality/%%SOPInstanceUID.dcm"%sorted_base_path]
+                   "%s"%raw_base_path, "%s/%s"%(sorted_base_path, sorted_structure)]
 
   bash_return = subprocess.run(bash_command, check = True, text = True)
 
